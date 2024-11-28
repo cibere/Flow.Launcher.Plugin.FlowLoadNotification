80df101a3a334fce8a3971aaf388b5b9465993dc
cibere <71997063+cibere@users.noreply.github.com>
2024-11-27 16:03:26 -0800

    run black

--- Plugin/core.py
+++ Plugin/core.py
@@ -2,9 +2,13 @@ from flogin import Plugin
 
 plugin = Plugin()
 
+
 @plugin.event
 async def on_initialization():
     for keyword in plugin.metadata.keywords:
         await plugin.metadata.remove_keyword(keyword)
-        
-    await plugin.api.show_notification("Flow Load Notification", "Flow Load Notification has loaded. Flow Launcher should finish loading monetarily.")
+
+    await plugin.api.show_notification(
+        "Flow Load Notification",
+        "Flow Load Notification has loaded. Flow Launcher should finish loading monetarily.",
+    )
