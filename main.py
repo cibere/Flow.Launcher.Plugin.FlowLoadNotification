a15433bf7727225623c7aff111afd67650102409
cibere <71997063+cibere@users.noreply.github.com>
2025-09-07 12:07:55 -0700

    Refactor plugin structure and update build workflow
    Moved image assets to the 'assets' directory and updated references. Deleted Plugin/core.py and generate_plugin_file.py, consolidating plugin logic into main.py. Improved main.py to handle plugin initialization and notification, with logging for disabled state. Updated plugin.json metadata and version. Replaced custom build steps in GitHub Actions with FlowBuildPluginAction, and added pyproject.toml for linting and type checking configuration. Added aioconsole to requirements.txt.

--- main.py
+++ main.py
@@ -4,9 +4,32 @@ import sys
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, "lib"))
-sys.path.append(os.path.join(parent_folder_path, "plugin"))
+sys.path.append(os.path.join(parent_folder_path, ".venv", "lib", "site-packages"))
 
-from Plugin.core import plugin
+import logging
 
-if __name__ == "__main__":
-    plugin.run()
+from flogin import Plugin
+
+log = logging.getLogger("plugin")
+
+plugin = Plugin()
+
+
+@plugin.event
+async def on_initialization() -> None:
+    for keyword in plugin.metadata.keywords:
+        await plugin.metadata.remove_keyword(keyword)
+
+    if plugin.metadata.disabled:
+        log.info("plugin disabled, not sending notification")
+    else:
+        await plugin.api.show_notification(
+            "Flow Load Notification",
+            "Flow Load Notification has loaded. Flow Launcher should finish loading monetarily.",
+        )
+        log.info("Notification sent")
+
+    sys.exit()
+
+
+plugin.run()
